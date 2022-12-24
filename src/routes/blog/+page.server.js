3f1030dc30decb52dfa20873e90169e57f9f5fde
Kiran S <codehub.kirans@gmail.com>
2022-12-25 01:21:56 +0530

    Modified prerender options

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -1,6 +1,3 @@
-// Ensures all pages under this layout (which is all of them) are statically prerendered at build time 
-export const prerender = false
-
 export const load = async ({ url, fetch }) => {
 	const postRes = await fetch(`${url.origin}/api/posts.json`)
 	const posts = await postRes.json()
