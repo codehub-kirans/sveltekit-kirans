f5107e5d84489d9eb61e27f7d7e5435671056976
Kiran S <codehub.kirans@gmail.com>
2022-12-25 01:10:04 +0530

    Enabled prerender

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,20 @@
 <!-- This page handles any error encountered by the site. -->
 <script>
-import { page } from '$app/stores'
+	import { page } from '$app/stores'
+	const emojis = {
+		// TODO add the rest!
+		404: '🫠',
+		500: '💥'
+	};
 </script>
 
-<h2>{$page.status}</h2>
-<p class="subhead">{$page.error.message}</p>
+<h2>{$page.status} | {$page.error.message} </h2>
+<span style="font-size: 5em">
+	{emojis[$page.status] ?? emojis[500]}
+</span>
 
-<p><strong>Sorry!</strong> Maybe try one of these links?</p>
+<p><strong>Sorry!</strong> The page you're looking for could not be found. Maybe try one of these links?</p>
 <ul>
 	<li><a href="/">Home</a></li>
+	<li><a href="/contact">Contact</a></li>
 </ul>
